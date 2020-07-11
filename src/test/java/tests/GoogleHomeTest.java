4d55e32116c3f747f003aa585beef696a3f3cc80
moaidmoatasem <47253204+moaidmoatasem@users.noreply.github.com>
2020-07-11 19:10:02 +0200

    Update GoogleHomeTest.java

--- src/test/java/tests/GoogleHomeTest.java
+++ src/test/java/tests/GoogleHomeTest.java
@@ -1,33 +1,22 @@
 package tests;
 
-import org.omg.CORBA.TIMEOUT;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.GoogleHomePage;
-import pages.SearchResultPage;
-
-import java.sql.Time;
-import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
 
 public class GoogleHomeTest extends TestBase {
 
     public GoogleHomePage googleHomePageObj;
-    //SearchResultPage searchResultPageObj;
 
     //asserting the title
     @Test (priority = 1)
     public void verifyTitleIsGoogle(){
+        googleHomePageObj = new GoogleHomePage(driver);
         Assert.assertEquals("Google",driver.getTitle());
     }
 
-    @Test (priority = 2, dependsOnMethods = {"titleIsGoogle"})
+    @Test (priority = 2, dependsOnMethods = {"verifyTitleIsGoogle"})
     public void verifyGoogleLogoIsDisplayed(){
         googleHomePageObj =new GoogleHomePage(driver);
         Assert.assertTrue(googleHomePageObj.homeGoogleLogo.isDisplayed());
@@ -36,11 +25,13 @@ public class GoogleHomeTest extends TestBase {
     @Test (priority = 3, dependsOnMethods = {"verifyTitleIsGoogle","verifyGoogleLogoIsDisplayed"})
     public void verifyFirstChoiceIsTypicalSameYouType() {
         googleHomePageObj = new GoogleHomePage(driver);
-        googleHomePageObj.userSearching("instabug");
-
+        String searchTxt = "instabug";
+        googleHomePageObj.userSearching(searchTxt);
+        int pageNum = 0;
+        String expectedText = "instabug";
 //        try {
-            String firstSuggestion= googleHomePageObj.autoCompleteSearchList.get(0).getText();
-            Assert.assertEquals(firstSuggestion,"instabug");
+            String firstSuggestion= googleHomePageObj.autoCompleteSearchList.get(pageNum).getText();
+            Assert.assertEquals(firstSuggestion,expectedText);
 //            System.out.println(firstSuggestion);
 //        }
 //        catch (Exception e) {
@@ -48,20 +39,26 @@ public class GoogleHomeTest extends TestBase {
 //        }
     }
 
-    @Test (priority = 4, dependsOnMethods = {"firstChoiceIsTheSameYouType"})
+    @Test (priority = 4, dependsOnMethods = {"verifyFirstChoiceIsTypicalSameYouType"})
     public void verifyAllSuggestionsHaveSameWord(){
-//        try {
+
             /*          or normal for loop
         for (int i = 0; i <= googleHomePageObj.autoCompleteSearchList.size()-1; i++) {
 */
-            for (WebElement webElement: googleHomePageObj.autoCompleteSearchList) {
+        googleHomePageObj = new GoogleHomePage(driver);
+        for (WebElement webElement: googleHomePageObj.autoCompleteSearchList) {
                 String s= webElement.getText();
-                System.out.println(s);
-                Assert.assertTrue(s.contains("instabug"));
+//                System.out.println(s);
+                String expectedText = "instabug";
+                Assert.assertTrue(s.contains(expectedText));
             }
-//        }
-//        catch (Exception e) {
-//            System.out.println("Error happened:"+e.getMessage());
-//        }
+
+    }
+    @Test (priority = 5)
+    public void verifyCountry(){
+        googleHomePageObj = new GoogleHomePage(driver);
+//        System.out.println(googleHomePageObj.country.getText());
+        String expectedText = "Egypt";
+        Assert.assertEquals(googleHomePageObj.country.getText(),expectedText);
     }
 }
